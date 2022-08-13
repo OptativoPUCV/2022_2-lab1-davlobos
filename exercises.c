f10814ff0f94bdb2a009fa2295c5bde1a47da294
Replit user <>
2022-08-13 18:47:42 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -81,7 +81,7 @@ typedef struct {
 
 Vector * crearVector(int n) {
   Vector* s=(Vector*)malloc(sizeof(Vector));
-  s->datos=(int*)calloc(n,sizeof(int))
+  s->datos=(int*)calloc(n,sizeof(int));
   s->capacidad=n;
   return s;
 }
