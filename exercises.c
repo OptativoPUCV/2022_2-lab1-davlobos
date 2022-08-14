3fdd7d136887bade07509b3fcb8a3d3f644ee016
Replit user <>
2022-08-14 18:13:30 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -112,7 +112,13 @@ Función que suma los vectores `a` y `b` y
 actualiza el vector `c` con el resultado de la suma.
 */
 void sumaV(Vector * a, Vector * b, Vector * c) {
-  *c=*(a+b);
+  int n=c->capacidad;
+  int x,y;
+  for(int i=0;i<n;i++){
+    x=a->datos[i];
+    y=b->datos[i];
+    c->datos[i]=x+y;
+  }
 }
 
 /*
@@ -121,6 +127,11 @@ Use las operaciones implementadas de vectores para
 sumar (a1,a2)+(b1,b2). Almacene el resultado en el vector c.
 */
 void sumaV2(int a1, int a2, int b1, int b2, Vector *c){
-  *c[0]=a1+a2;
-  *c[1]=b1+b2;
+  for(int i=0;i<2;i++){
+    if(i=0){
+      c->datos[i]=a1+b1;
+    }else{
+      c->datos[i]=a2+b2;
+    }
+  }
 }
